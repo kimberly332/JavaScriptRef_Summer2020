db12a3320cb060c6b0da43d437c77c17c5a60aa7
kimberly332 <kimberly.wang1010@gmail.com>
2020-07-24 12:20:54 -0400

    uploaded objects example

--- js/main.js
+++ js/main.js
@@ -1,43 +1,19 @@
-// this is an anonymous self invoking function
-// also called an IIFE (immediately invoked function expression)
-// also called a module (module programming pattern)
+let person = {
+  name: "tervor",
+  role: "troll",
+  height: "180",
+  hair: "fabulous",
+  eyeColor: "blue",
+  textLabel: document.querySelector('p')
+}
 
-(() => {
-  console.log("fried!");
+let car = {
+  wheels: 4,
+  make: "Honda",
+  model: "Pilot",
+  mileage: 400,
+  year: 2012,
+  fuel: 20
+}
 
-  let theButton = document.querySelector('button'),
-      theHeading = document.querySelector('h1');
-
-  function sayHello(message) {
-    console.log(message);
-  }
-
-  // get the ID of any element
-  function retrieveId() {
-    console.log(this.id);
-
-    // pass the ID to another function as an argument
-    displayTheId(this.id);
-  }
-
-  // display the ID of any clicked element on the page
-  function displayTheId(theId) {
-    document.querySelector('p').textContent = `You clicked on the element with ID of ${theId}`;
-  }
-
-  // this function will take a number, and operator, and a number
-  // and log the result
-  function sumNums(x, y) {
-    console.log(x + y);
-  }
-
-  // pass an argument to the function
-  sayHello("hi andres, i'm an argument.");
-
-  sumNums(5, 6);
-  sumNums(123, 67);
-
-  // event handling here
-  theButton.addEventListener("click", retrieveId);
-  theHeading.addEventListener("click", retrieveId);
-})();
+document.querySelector('p').textContent = `My name is ${person.name} and i am a ${person.role}`;
